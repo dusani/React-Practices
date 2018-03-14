3eeeec78c21da19bfa8bf27e61bbc7798a28342d
Daniel Usani <daniel.e.usani@gmail.com>
2018-03-14 09:40:25 -0400

    Added Components

---
+++ user-input-output/src/components/UserOutput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserOutput = props => {
+  return (
+    <div>
+      <p>{props.username}</p>
+      <p>Cras mattis consectetur purus sit amet fermentum.</p>
+      <p>Etiam porta sem malesuada magna mollis euismod.</p>
+    </div>
+  );
+};
+
+export default UserOutput;
